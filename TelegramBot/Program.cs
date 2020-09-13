81a3b534bd1815167d990a7ad0c0ddd3e38641ff
Baylak <baylak@MacBook-Pro.local>
2020-09-13 21:35:10 +0300

    add Telegram bot

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string token = "1113687191:AAGDtRxsVtYlr_eEv52fweYYvBW_8XIRpTg";
+            TelegramBotClient botClient = new TelegramBotClient(token);
+            Console.WriteLine(botClient.GetMeAsync().Result.FirstName);
+        }
+    }
+}
