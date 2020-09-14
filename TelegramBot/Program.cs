2ff1fd2983c43216e4cdfb2bd599dfa742448d38
Baylak <baylak@MacBook-Pro.local>
2020-09-14 07:16:22 +0300

    add telegram bot echo

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using Telegram.Bot;
+using System.IO;
 
 namespace TelegramBot
 {
-    class MainClass
+    class Program
     {
         public static void Main(string[] args)
         {
-            string token = "1113687191:AAGDtRxsVtYlr_eEv52fweYYvBW_8XIRpTg";
+            string token = File.ReadAllText(@"/Users/baylak/Projects/TelegramBot/TelegramBot/token.txt");
             TelegramBotClient botClient = new TelegramBotClient(token);
-            Console.WriteLine(botClient.GetMeAsync().Result.FirstName);
+            Console.WriteLine($"@{botClient.GetMeAsync().Result.FirstName} start!");
+
+            botClient.OnMessage += (s, arg) =>
+            {
+                Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
+                botClient.SendTextMessageAsync(
+                    chatId: arg.Message.Chat.Id,
+                    text: $"Получено. {arg.Message.Text}",
+                    replyToMessageId: arg.Message.MessageId
+                    );
+            };
+            botClient.StartReceiving();
+            Console.ReadLine();
         }
     }
 }
