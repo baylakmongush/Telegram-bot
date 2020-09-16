bbf26c31cb8211e2d04ff7413a2a12806984bc93
Baylak <baylak@MacBook-Pro.local>
2020-09-16 11:41:56 +0300

    add text and sticker for bot

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -8,21 +8,29 @@ namespace TelegramBot
     {
         public static void Main(string[] args)
         {
-            string token = File.ReadAllText(@"/Users/baylak/Projects/TelegramBot/TelegramBot/token.txt");
+            string token = "1185273900:AAGIHqz13UiyYsXNg3Mi2AtZAjxrpBiNfR4";
             TelegramBotClient botClient = new TelegramBotClient(token);
             Console.WriteLine($"@{botClient.GetMeAsync().Result.FirstName} start!");
 
             botClient.OnMessage += (s, arg) =>
             {
                 Console.WriteLine($"{arg.Message.Chat.FirstName}: {arg.Message.Text}");
-                botClient.SendTextMessageAsync(
-                    chatId: arg.Message.Chat.Id,
-                    text: $"Получено. {arg.Message.Text}",
-                    replyToMessageId: arg.Message.MessageId
+                if (arg.Message.Text == "я Хлебушек")
+                botClient.SendStickerAsync(
+                chatId: arg.Message.Chat,
+                sticker: "https://github.com/TelegramBots/book/raw/master/src/docs/sticker-fred.webp"
+                );
+                else
+                {
+                    botClient.SendTextMessageAsync(
+                    chatId: arg.Message.Chat,
+                    text: "Ты не Хлебушек"
                     );
+                }
             };
             botClient.StartReceiving();
             Console.ReadLine();
+            botClient.StopReceiving();
         }
     }
 }
